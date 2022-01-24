65d26f24cc849083baf18f7aa01f7890e76cc1e1
Jacky Avenido <jackyavenido.se@gmail.com>
2022-01-24 13:26:37 +0800

    doctors and about us

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MainLayout from "components/Layouts/MainLayout";
 import PageLayout from "components/Layouts/PageLayout";
 import { getPostByCategory } from "lib/api";
@@ -7,6 +7,13 @@ import { urlObjectKeys } from "next/dist/shared/lib/utils";
 import PageHeaderLayout from "components/Layouts/PageHeaderLayout";
 import CardAbout from "./CardAbout";
 import { aboutUsMenu } from "constants/navbarmenu";
+import { Document, Page } from 'react-pdf';
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+import * as pdfjs from 'pdfjs-dist'
+import Pagination from '@mui/material/Pagination';
+import Polygon from 'components/Layouts/polygon';
+import Button from '@mui/material/Button';
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 export default function VMGC({ nodesAbout }) {
   const vision = nodesAbout?.find((e) => e.slug == "vision");
@@ -20,10 +27,27 @@ export default function VMGC({ nodesAbout }) {
 
   console.log("about", about);
 
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  function onDocumentChange(type) {
+
+    if (pageNumber <= numPages) {
+      setPageNumber(type == "prev" ? pageNumber - 1 : pageNumber + 1);
+    }
+
+    console.log("type", type)
+  }
+
+
   return (
     <>
       <MainLayout type="index">
-        <PageHeaderLayout />
+        <PageHeaderLayout title="ABOUT US" />
         <section className="pb-20 bg-blueGray-200 -mt-24">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap">
@@ -74,6 +98,10 @@ export default function VMGC({ nodesAbout }) {
               </div>
             </div>
 
+
+
+
+
             <div className="flex flex-wrap items-center mt-32">
               <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
                 <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
@@ -82,7 +110,7 @@ export default function VMGC({ nodesAbout }) {
                 <h3 className="text-3xl mb-2 font-semibold leading-normal">
                   {about.title}
                 </h3>
-                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
+                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600" style={{ textAlign: 'justify' }} >
                   {about?.content ? Parser(about?.content) : <></>}
                 </p>
               </div>
@@ -118,19 +146,9 @@ export default function VMGC({ nodesAbout }) {
               </div>
             </div>
             <div className="flex flex-wrap items-center mt-32">
-              <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
-                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <i className="fas fa-hospital-alt text-xl"></i>
-                </div>
-                <h3 className="text-3xl mb-2 font-semibold leading-normal">
-                  {history.title}
-                </h3>
-                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
-                  {history?.content ? Parser(history?.content) : <></>}
-                </p>
-              </div>
 
-              <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
+
+              {/* <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blueGray-700">
                   <img
                     alt="..."
@@ -158,10 +176,63 @@ export default function VMGC({ nodesAbout }) {
                     </p>
                   </blockquote>
                 </div>
+              </div> */}
+
+              <div className="w-full md:w-5/10 px-4 mr-auto ml-auto">
+                {/* <div className="w-full md:w-5/12 px-4 mr-auto ml-auto"> */}
+                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                  <i className="fas fa-hospital-alt text-xl"></i>
+                </div>
+                <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                  {history.title}
+                </h3>
+                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600" style={{ textAlign: 'justify' }}>
+                  {history?.content ? Parser(history?.content) : <></>}
+                </p>
               </div>
+
             </div>
           </div>
+
+
+
         </section>
+
+        <section className="pb-20  -mt-24">
+          <Polygon divClassName={"top-auto  bottom-0 left-0 right-0 w-full  pointer-events-none overflow-hidden h-16"} />
+
+          <center>
+            <div className="bg-white">
+              <b style={{ fontSize: '25px'}}>Simplified Registration Statement For Hospitals</b> <br/>
+              <div className="shadow-md" style={{width: '40%'}}>
+                <Document
+                  // file="http://admin.acemcbohol.ph/wp-content/uploads/2022/01/AMENDED-PREL-SRS-ACEMC-BOHOL-5.15-1.pdf"
+                  file="/files/AMENDED-PREL-SRS-ACEMC-BOHOL-5-15-1.pdf"
+                  onLoadSuccess={onDocumentLoadSuccess}
+                >
+                  <Page pageNumber={pageNumber} width={600} />
+                </Document>
+              </div>
+              
+              <p>Page {pageNumber} of {numPages}</p>
+              <div style={{ fontSize: '25px', cursor: "pointer" }}>
+                <i class="fas fa-arrow-circle-left" onClick={(e) => {
+                  onDocumentChange("prev")
+                }}></i>
+                &nbsp;&nbsp;
+                <i class="fas fa-arrow-circle-right" onClick={(e) => {
+                  onDocumentChange("next")
+                }}></i>
+              </div>
+            </div>
+
+           <a href="http://admin.acemcbohol.ph/wp-content/uploads/2022/01/AMENDED-PREL-SRS-ACEMC-BOHOL-5.15-1.pdf" target="_blank"> <Button variant="contained">Open</Button></a>
+
+          </center>
+
+
+        </section>
+
       </MainLayout>
     </>
   );
