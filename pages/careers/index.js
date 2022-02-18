85002c68475bab2a93d07658d60496cc560fcad7
Jacky Avenido <jackyavenido.se@gmail.com>
2022-02-18 10:44:05 +0800

    clearing

--- pages/careers/index.js
+++ pages/careers/index.js
@@ -4,22 +4,8 @@ import CardExpand from "components/Cards/CardExpandCareers";
 import { getPostByCategory } from "lib/api";
 import PageHeaderLayout from "components/Layouts/PageHeaderLayout";
 export default function VMGC(props) {
- 
 
   const careers = props?.NodeNewsAndEvent;
-
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-  
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-  function onDocumentChange(type) {
-    if (pageNumber <= numPages) {
-      setPageNumber(type == "prev" ? pageNumber - 1 : pageNumber + 1);
-    }
-  }
-
 
   return (
     <>
@@ -28,13 +14,13 @@ export default function VMGC(props) {
         <section className="pb-10 bg-blueGray-200 -mt-24">
           <div className="">
             <div className="flex flex-wrap items-center mt-10">
-              <div className="w-full md:w-5/10 px-4 mr-auto ml-auto" style={{padding:'8vw',width:'80%'}}>
+              <div className="w-full md:w-5/10 px-4 mr-auto ml-auto" style={{ padding: '8vw', width: '80%' }}>
                 <h3 className="text-3xl mb-2 font-semibold leading-normal">
-                  <br/>
-                FOR THE INTERESTED APPLICANTS
+                  <br />
+                  FOR THE INTERESTED APPLICANTS
                 </h3>
                 <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600" style={{ textAlign: 'justify' }}>
-                You may email your resume to hr@acemcbohol.com or you may personally submit it to our office at 8th floor HR Department ACE Medical Center-Bohol 0368 AS Bioco St., Carlos P. Garcia East Avenue, Mansasa District, Tagbilaran City, Bohol 6300.
+                  You may email your resume to hr@acemcbohol.com or you may personally submit it to our office at 8th floor HR Department ACE Medical Center-Bohol 0368 AS Bioco St., Carlos P. Garcia East Avenue, Mansasa District, Tagbilaran City, Bohol 6300.
                 </p>
               </div>
 
@@ -46,19 +32,19 @@ export default function VMGC(props) {
         </section>
         <center>
           <div className="w-full md:w-5/10 px-4 mr-auto ml-auto">
-        {careers.map((p, index) => (
-            <div style={{width:'90%',padding:'20px',justifyContent:'center'}}>
+            {careers.map((p, index) => (
+              <div style={{ width: '90%', padding: '20px', justifyContent: 'center' }}>
                 <CardExpand
                   title={p.title}
                   content={p?.content}
                 />
               </div>
-             ))}
-            </div>
-          </center>
-        <br/>
-        <br/>
-        <br/>
+            ))}
+          </div>
+        </center>
+        <br />
+        <br />
+        <br />
       </MainLayout>
     </>
   );
